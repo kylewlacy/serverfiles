c890682d4a18dfe45668d003fbd0fc3502d8c44b
Kyle Lacy <kylewlacy@me.com>
2012-10-23 05:43:33 -0700

    update pacman script

--- root/.pacman/man.sh
+++ root/.pacman/man.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sf="--work-tree=/ --git-dir=$HOME/.serverfiles.git"
 if [ -z "$1" ]; then
   action="backup"
 else
@@ -6,34 +7,34 @@ else
 fi
 
 if [ -z "$2" ]; then
-  latest=$(git sf log --pretty="format:%H" --author=pacman -n1)
+  latest=$(git $sf log --pretty="format:%H" --author=pacman -n1)
 else
   latest=$2
 fi
 if [ -z "$3" ]; then
-  prior=$(git sf log --pretty="format:%H" --author=pacman -n2 | sed -n 2p)
+  prior=$(git $sf log --pretty="format:%H" --author=pacman -n2 | sed -n 2p)
 else
   prior=$3
 fi
 
 if [ "$action" == "backup" ]; then
   echo "==> Backing up..."
-  git sf stash
+  git $sf stash
   pacman -Qq > packages.txt
   pacman -Qi > packages_verbose.txt
 
-  git sf add "$HOME/.pacman/packages.txt"
-  git sf add "$HOME/.pacman/packages_verbose.txt"
+  git $sf add "$HOME/.pacman/packages.txt"
+  git $sf add "$HOME/.pacman/packages_verbose.txt"
 
-  git sf commit --author="Pacman Backup Bot <kylewlacy@me.com>" -m "pacman backup at $(date +'%x %T%z')" > /dev/null
-  git sf stash pop > /dev/null 2>&1
+  git $sf commit --author="Pacman Backup Bot <kylewlacy@me.com>" -m "pacman backup at $(date +'%x %T%z')" > /dev/null
+  git $sf stash pop > /dev/null 2>&1
 elif [ "$action" == "restore" ]; then
   echo "==> Restoring to $latest..."
-  git sf checkout $latest -- "$HOME/.pacman/packages.txt"
+  git $sf checkout $latest -- "$HOME/.pacman/packages.txt"
 elif [ "$action" == "list" ]; then
   echo "listing $latest"
-  git sf show $latest:"$HOME/.pacman/packages.txt"
+  git $sf show $latest:"$HOME/.pacman/packages.txt"
 elif [ "$action" == "diff" ]; then
   echo "==> Diffing between $prior and $latest..."
-  git sf diff $prior..$latest -- "$HOME/.pacman/packages.txt"
+  git $sf diff $prior..$latest -- "$HOME/.pacman/packages.txt"
 fi
